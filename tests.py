9b2c77bf42c504d37bfa2e02f030b254f7be408f
servrcube <63092977+servrcube@users.noreply.github.com>
2024-10-03 21:25:51 +0200

    Created full test suite

--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+# pylint: disable=all
 ### Basic imports
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,7 +14,10 @@ from pstats import Stats, SortKey
 from config import Config
 from sim_quadtree import Quadtree
 from sim_grid import StdRes
-from sim_upwind_simple import Simulation
+
+from sim_upwind_simple import Simulation as no_tvd_simulation
+from sim_upwind_tvd import Simulation as tvd_simulation
+
 from gen_quadtree import (
     Tree,
     write_nodes_to_file,
@@ -21,7 +25,7 @@ from gen_quadtree import (
     write_sqmatrix_to_file,
 )
 
-def depth_formula(x, y, max_depth, min_depth, center_x=0.5, center_y=0.5):
+def depth_formula_sedov(x, y, max_depth, min_depth, center_x=0.5, center_y=0.5):
     # default is proximity to center
     dist_to_center = ((x - center_x) ** 2 + (y - center_y) ** 2) ** 0.5
     if dist_to_center <= 0.2:
@@ -35,11 +39,81 @@ def depth_formula(x, y, max_depth, min_depth, center_x=0.5, center_y=0.5):
     else: 
         return min_depth
 
-def gen_quadtree(width, height, min_depth, max_depth, max_resolution, files=None):
+def depth_formula_sod(x, y, max_depth, min_depth, center_x=0.5, center_y=0.5):
+    # Rotation matrix
+    # | cos(θ) -sin(θ) |
+    # | sin(θ)  cos(θ) |
+
+    theta = np.pi * 1.25
+    M = np.array([[np.cos(theta), -np.sin(theta)],[np.sin(theta),np.cos(theta)]])
+    center = np.array([center_x, center_y])
+    rot = (np.array([x, y]) - center) @ M + center 
+
+    if rot[1] < 0.1:
+        return min_depth
+    if rot[1] < 0.55 and (0.6 >= rot[0] >= 0.4):
+        return max_depth
+
+    if 0.6 >= rot[0] >= 0.4:
+        return max_depth
+
+    if 0.67 >= rot[0] >= 0.33:
+        return max_depth-2
+
+    if rot[1] < 0.6:
+        return max_depth-2
+    
+    return min_depth
+
+
+def faulty1(x, y, max_depth, min_depth, center_x=0.5, center_y=0.5):
+    # Rotation matrix
+    # | cos(θ) -sin(θ) |
+    # | sin(θ)  cos(θ) |
+
+    theta = np.pi * 1.25
+    M = np.array([[np.cos(theta), -np.sin(theta)],[np.sin(theta),np.cos(theta)]])
+    center = np.array([center_x, center_y])
+    rot = (np.array([x, y]) - center) @ M + center 
+    # print(rot)
+    if rot[1] < 0.1:
+        return min_depth
+    if rot[1] < 0.55:
+        return max_depth
+
+    if 0.6 >= rot[0] >= 0.4:
+        return max_depth
+
+    if 0.67 >= rot[0] >= 0.33:
+        return max_depth-2
+
+    if rot[1] < 0.6:
+        return max_depth-2
+
+    return min_depth
+
+
+def faulty2(x, y, max_depth, min_depth, center_x=0.5, center_y=0.5):
+    # Rotation matrix
+    # | cos(θ) -sin(θ) |
+    # | sin(θ)  cos(θ) |
+
+    theta = np.pi * 1.25
+    M = np.array([[np.cos(theta), -np.sin(theta)],[np.sin(theta),np.cos(theta)]])
+    center = np.array([center_x, center_y])
+    rot = (np.array([x, y]) - center) @ M + center 
+
+    if 0.67 >= rot[0] >= 0.33:
+        return max_depth-2
+
+    return min_depth
+
+
+def gen_quadtree(width, height, min_depth, max_depth, max_resolution, depth_func, files=None):
     logging.info(
         f"Generating quadtree with min_depth={min_depth}, max_depth={max_depth}, max_resolution={max_resolution}"
     )
-    qt = Tree(width, height, min_depth, max_depth, depth_formula=depth_formula)
+    qt = Tree(width, height, min_depth, max_depth, depth_formula=depth_func)
 
     logging.info("Generating helper constructs")
     # Generate nodes and neighbors
@@ -115,6 +189,53 @@ def sedov_taylor_blast(res):
     )
 
     return cfg, target_time
+def EOS(res, 
+        rl, ul, pl,
+        rr, ur, pr, 
+        t, gamma):
+
+    target_time = t
+
+    s = np.array(res)  # space resolution
+    L = np.triu_indices(s[0])
+    U_mask = np.zeros(s, dtype=bool)
+    U_mask[:] = True
+    U_mask[L] = False
+
+    density = np.ones(s) * rr
+    density[U_mask] = rl
+
+    if ul != 0:
+        ul = ul/np.sqrt(2)
+
+    if ur != 0:
+        ul = ul/np.sqrt(2)
+    
+    velocity = np.ones((2,*s)) * ur
+    velocity[:,U_mask] = ul
+
+    pressure = np.ones(s) * pr
+    pressure[U_mask] = pl
+
+    energy = pressure / (gamma - 1)
+
+    cfg = Config(
+        vol=np.sqrt([0.5, 0.5]),
+        steps=s,
+        list_p=density,
+        list_v=velocity,  # contains both x and y
+        list_e=energy,
+        dt=0.0001,
+        gamma=gamma,
+        spherical=False,
+    )
+
+    return cfg, target_time
+
+def sod_shock_tube(res):
+    return EOS(res, rl=1.0,   ul=0.,   pl=1.0,
+                    rr=0.125, ur=0.,   pr=0.1,
+                    t = 0.168, gamma = 1.4)
 
 def range_incl(start,stop,step):
     n = start
@@ -125,66 +246,28 @@ def range_incl(start,stop,step):
     
     yield stop-1
 
-def main():
-    # Output directory
-    output_dir = "simulation_outputs"
-
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-        print(f"Created output directory: {output_dir}")
-
-
-    # Configure logging
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(levelname)s:%(module)s:%(funcName)s | %(message)s', filename=f"{output_dir}/log.log")
-
-
-
-    width, height = 1, 1  # dimensions of entire area
-    min_depth = 1
-
-    start_depth = 5
-    end_depth = 13
-    step = 64
-    step2 = 256
-    step3 = 1024
-
-    # Resolutions for StdRes
-    std_resolutions =  list(range(2**start_depth, 2**8+1, step)) +  list(range(2**8, 2**11+1, step2)) + list(range_incl(2**11, 2**end_depth+1, step3))
-    # std_resolutions = range(0) # no iter
-
-    # Resolutions for Quadtree (powers of 2)
-    quadtree_resolutions = [2**i for i in range(start_depth, end_depth+1)]
-    # quadtree_resolutions = range(0) # no iter
-
-    do_no_iter = False
-
-
-    # Initialize lists to store profiling times
-    std_times = []
-    quadtree_times = []
+def run_std_res_tests(simulationf, resolutions, output_dir, do_no_iter, test, std_times):
 
     # Run simulations for StdRes
     logging.info("Starting simulations for StdRes")
 
-    with open(f"{output_dir}/sim_info.txt","w+"): pass
-
     with open(f"{output_dir}/sim_info.txt","a") as f:
         f.write("STDRES\nres total_time\n")
 
-    for res in std_resolutions:
+    for res in resolutions:
         logging.info(f"Running StdRes simulation at resolution {res}")
-        cfg, target_time = sedov_taylor_blast((res, res))
+        cfg, target_time = test((res, res))
 
         # Setup space
         space = StdRes(cfg)
 
         # Initial variables
-        simulation = Simulation(space)
+        simulation = simulationf(space)
 
         profile_filename = path.join(output_dir, f"stdres_res{res}_profile.prof")
         with Profile() as profile:
             # Run simulation
-            all_states, all_times = simulation.run(target_time, checkpoint_freq=500, verbose=True, no_iter=do_no_iter)
+            all_states, all_times = simulation.run(target_time, checkpoint_freq="auto", verbose=True, no_iter=do_no_iter)
             profile.dump_stats(profile_filename)
             stats = Stats(profile)
             total_time = stats.total_tt
@@ -204,33 +287,34 @@ def main():
         with open(f"{output_dir}/sim_info.txt","a") as f:
             f.write(f"{res} {total_time}\n")
 
+def run_quad_res_tests(simulationf, resolutions, output_dir, do_no_iter, test, width, height, min_depth, quadtree_times, depth_func):
     # Run simulations for Quadtree
     logging.info("Starting simulations for Quadtree")
 
     with open(f"{output_dir}/sim_info.txt","a") as f:
         f.write("QUADTREE\nres total_time\n")
 
-    for res in quadtree_resolutions:
+    for res in resolutions:
         logging.info(f"Running Quadtree simulation at resolution {res}")
         max_resolution = res
         # Generate Quadtree
-        qtd_dir = f"Qtdata_res{res}"
+        qtd_dir = f"{output_dir}/Qtdata_res{res}"
         if not os.path.exists(qtd_dir):
             os.makedirs(qtd_dir)
-        gen_quadtree(width, height, min_depth, int(np.log2(max_resolution)), max_resolution, files=qtd_dir)
+        gen_quadtree(width, height, min_depth, int(np.log2(max_resolution)), max_resolution, depth_func, files=qtd_dir)
 
-        cfg, target_time = sedov_taylor_blast((max_resolution, max_resolution))
+        cfg, target_time = test((max_resolution, max_resolution))
 
         # Setup space
         space = Quadtree(cfg, file_path=qtd_dir)
 
         # Initial variables
-        simulation = Simulation(space)
+        simulation = simulationf(space)
 
         profile_filename = path.join(output_dir, f"quadtree_res{res}_profile.prof")
         with Profile() as profile:
             # Run simulation
-            all_states, all_times = simulation.run(target_time, checkpoint_freq=500, verbose=True, no_iter=do_no_iter)
+            all_states, all_times = simulation.run(target_time, checkpoint_freq="auto", verbose=True, no_iter=do_no_iter)
             profile.dump_stats(profile_filename)
             stats = Stats(profile)
             total_time = stats.total_tt
@@ -250,10 +334,113 @@ def main():
         np.savez_compressed(save_name, all_states=converted_states, all_times=all_times)
         logging.info(f"Simulation results saved to {save_name}")
 
-    # Save profiling times
-    profiling_times_file = path.join(output_dir, "profiling_times.npz")
-    np.savez_compressed(profiling_times_file, std_res=std_resolutions, std_times=std_times, quadtree_res=quadtree_resolutions, quadtree_times=quadtree_times)
-    logging.info(f"Profiling times saved to {profiling_times_file}")
+def main():
+    # Tests to do:
+    # Blast:
+    # 1: 32 -> 512
+    # NO TVD/ TVD
+    #     STDRES / QUADTREE
+    # Tube:
+    # No TVD/TVD:
+    #   STDRES
+    # No TVD/TVD:
+    #   Quad: proper
+    #   Quad: faulty1
+    #   Quad: faulty2
+
+    # Configure logging 
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(levelname)s:%(module)s:%(funcName)s | %(message)s', filename=f"log.log")
+
+    step = 64
+
+    width, height = 1, 1  # dimensions of entire area
+    min_depth = 1
+
+    start_depth = 5
+    end_depth = 9
+    step = 64
+
+    do_no_iter = True
+
+    std_resolutions = sorted(list(set(list(range_incl(2**start_depth, 2**end_depth+1, step)) + [2**i for i in range(start_depth, end_depth+1)])))
+    quadtree_resolutions = [2**i for i in range(start_depth, end_depth+1)]
+
+
+    combinations = [
+        # folder, space-type, resolutions, tvd/no-tvd, test, depth_func
+        ## Blast
+        ### STDRES
+        ["std_blast_no_tvd", "std", std_resolutions, False, "blast", None],
+        ["std_blast_tvd", "std", std_resolutions, True, "blast", None],
+        ### QUAD
+        ["quad_blast_no_tvd", "quad", quadtree_resolutions, False, "blast", depth_formula_sedov],
+        ["quad_blast_tvd", "quad", quadtree_resolutions, True, "blast", depth_formula_sedov],
+
+        ## Tube
+        ### STDRES
+        ["std_tube_no_tvd", "std", std_resolutions, False, "tube", None],
+        ["std_tube_tvd", "std", std_resolutions, True, "tube", None],
+        ### QUAD: proper
+        ["quad_tube_no_tvd_proper", "quad", quadtree_resolutions, False, "tube", depth_formula_sod],
+        ["quad_tube_tvd_proper", "quad", quadtree_resolutions, True, "tube", depth_formula_sod],
+        ### QUAD: faulty1
+        ["quad_tube_no_tvd_faulty1", "quad", quadtree_resolutions, False, "tube", faulty1],
+        ["quad_tube_tvd_faulty1", "quad", quadtree_resolutions, True, "tube", faulty1],
+        ### QUAD: faulty2
+        ["quad_tube_no_tvd_faulty2", "quad", quadtree_resolutions, False, "tube", faulty2],
+        ["quad_tube_tvd_faulty2", "quad", quadtree_resolutions, True, "tube", faulty2],
+    ]
+
+    for (folder, space_type, resolutions, tvd, test, depth_func) in combinations:
+        
+        # Output directory
+        output_dir = folder
+
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+            print(f"Created output directory: {output_dir}")
+
+        times = []
+        log_text = ""
+
+        if test == "sod":
+            testf = sod_shock_tube
+            log_text += " Test: sod "
+        elif test == "blast":
+            testf = sedov_taylor_blast
+            log_text += " Test: blast "
+
+        if tvd:
+            simulationf = tvd_simulation
+            log_text += " TVD: True "
+        else:
+            simulationf = no_tvd_simulation
+            log_text += " TVD: False "
+
+        if space_type == "std":
+            runf_space = run_std_res_tests
+            args = (simulationf, resolutions, output_dir, do_no_iter, testf, times)
+            log_text += " Space: Grid "
+
+        elif space_type == "quad":
+            runf_space = run_quad_res_tests
+            args = (simulationf, resolutions, output_dir, do_no_iter, sod_shock_tube, width, height, min_depth, times, depth_func)
+            log_text += " Space: Quadtree "
+
+
+        logging.info(log_text)
+        #create file
+        with open(f"{output_dir}/sim_info.txt","w+"): pass
+
+        runf_space(*args)
+
+        # run_std_res_tests(std_resolutions, output_dir, do_no_iter, sod_shock_tube, std_times)
+        # run_quad_res_tests(quadtree_resolutions, output_dir, do_no_iter, sod_shock_tube, width, height, min_depth, quadtree_times, faulty2)
+
+        # Save profiling times
+        profiling_times_file = path.join(output_dir, "profiling_times.npz")
+        np.savez_compressed(profiling_times_file, res=resolutions, times=times)
+        logging.info(f"Profiling times saved to {profiling_times_file}")
 
 if __name__ == "__main__":
     main()
